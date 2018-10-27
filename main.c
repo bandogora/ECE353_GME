39a03d21830f7079e33dab1d69e324674f0dc9f7
Justin Forgue <juforgue@gmail.com>
2018-10-27 17:49:18 -0400

    Interrupts

--- main.c
+++ main.c
@@ -16,6 +16,11 @@ void USART_Init(unsigned int baud){
 
     //Initialize to 8 data bits 1 stop bit
     UCSRC = (1<<URSEL)|(3<<UCSZ0);
+
+}
+
+ISR(TIMER1_COMPA_vect){
+	PORTB=0x00;
 }
 
 void EEPROM_Write(unsigned int address, unsigned char data){
@@ -96,24 +101,6 @@ void USART_Flush(void){
 }
 
 
-unsigned int readTCNT1(void){
-    unsigned char sreg;
-    unsigned int i;
-
-    //Save global interrupt flag
-    sreg = SREG;
-
-    //Disable interuppts
-    //_CLI();
-
-    //Read TCNT1 into i
-    i = TCNT1;
-
-    //Restore global interuppt flag
-    SREG = sreg;
-
-    return i;
-}
 
 int main(void){
     //set baud rate
@@ -124,100 +111,83 @@ int main(void){
     DDRA = 0x00;            //Make all ports input
     DDRB = 0xFF;            //Make all ports output
     PORTB = 0x00;
-    TCCR1B |= (1 << CS11);  //Prescaler = 8
-    TCNT1 = 0;              //Clear count
-    TIMSK = (1 << TOIE1) ;  //Enable timer1 overflow interrupt(TOIE1)
-    sei();                  //Enable global interrupts
+
+    TCCR1B |= (1<<CS12);  //Prescaler = 256
+	TCNT1 = 0;			//Timer set to zero
+	TIMSK = (1<<OCIE1A); //Enable timer1 overflow interrupt(TOIE1)
+	//TIMSK = (1<<TOIE1); //Enable timer1 overflow interrupt(TOIE1)
+	OCR1A = 12499;
+
+	sei();//Enable global interrupts
+
 
     unsigned int writeAddress = 0;
     unsigned int readAddress = 0;
-    unsigned char data[8];
+    unsigned char data[3];
     unsigned int noteCount = 0;
+	unsigned int notePlay = 0;
+	uint16_t time = 0;
 
-    while(1){
-
-        if(isRecordOn()){
-/*
-        while((writeAdress<1022 && isRecordOn()){
-            for(int i = 0; i < 6; i++){
-                if(i==1){
-                    if(writeAddress > 1){
-                        time = TCNT1;
-                        EEPROM_Write(writeAddress, (time>>8));
-                        writeAddress++;
-                        EEPROM_Write(writeAddress, time);
-                        writeAddress++;
-                    }
-                TCTN1 = 0;
-                PORTB=data;
-                }
-            EEPROM_Write(writeAddress, data);
-            EEPROM_Write((1022), (address>>8));
-            EEPROM_Write((1023), address);
- 
-            writeAddress++;
-            }
-        noteCount++;
-        }
-    */
+    
+	while(1){
+		if(isRecordOn()){
+		USART_Init(0x07);
+			writeAddress = 0;
+			noteCount = 0;
+			TCNT1 = 0;
+			USART_Flush();
+            notePlay = 0;
+            time =0;
             //reset address at start of record
             //record until eeprom is full or is switched off
-            unsigned int time;
-            while((writeAddress<1024U) && isRecordOn()){
-                for(unsigned int i=0;i<6;i++){
+         	//unsigned int time;
+            while((writeAddress<1022U) && isRecordOn()){
+                for(unsigned int i=0;i<3;i++){
+                    //store each of the three MIDI bytes in EEPROM
+                    	if(i==0){
+						EEPROM_Write(1023,noteCount++);
+						time=TCNT1;
+						EEPROM_Write(writeAddress++,(time));
+                    	EEPROM_Write(writeAddress++,time>>8);
+						TCNT1 = 0;
+					}
 
-
-
-                    //store each of the six MIDI bytes in EEPROM
-                    data[i] = USART_Receive();
+					data[i] = USART_Receive();
                     EEPROM_Write(writeAddress,data[i]);
-
-
-                    //Store time data
-
-                    if(i==1){
-                        EEPROM_Write(writeAddress++,(time >> 8));
-                        EEPROM_Write(writeAddress++,time);
-
-                    }
-
-                    if(i==5){
-                        //data[7] = readTCNT1();
-                        EEPROM_Write(writeAddress++,TCNT1);
-                        time = TCNT1;
-                        //TCNT1 = 0;
-                    }
-
-
+					writeAddress++;
+					//at the end of the midi signal add the timing info to the next two bytes in eeprom
                     PORTB = data[1];
                 }
-                TCNT1 = 0;
             }
-
-            PORTB = 0;
         }
 
         if(isPlayOn()){
-            unsigned char startTime;
-            unsigned char endTime;
-
-            while (readAddress<1024U && isPlayOn()){
-                for(unsigned int j=0;j<6;j++){
-                    USART_Transmit(EEPROM_Read(readAddress));
-                    readAddress++;
+		    USART_Init(0x07);
+		//	TCNT1=0;
+		    readAddress = 2;
+			notePlay=0;
+			noteCount=EEPROM_Read(1023);
+			USART_Flush();
+			time = 0;
+            while (notePlay < noteCount && isPlayOn()){
+			    //read each of the three MIDI bytes from the EEPROM
+				for(unsigned int j=0;j<3;j++){
+                	data[j] = EEPROM_Read(readAddress);
+					USART_Transmit(EEPROM_Read(readAddress));
+					readAddress++;
                 }
+				//creating a 16 bit time value from 2 eeprom bytes (the second note)
+					time = EEPROM_Read(readAddress)|(8<<EEPROM_Read(readAddress+1));
+					//delay by found time
+					_delay_ms(time);
+					readAddress+=2;
 
-                startTime = EEPROM_Read(readAddress++);
-                endTime = EEPROM_Read(readAddress++);
-                //printf("%s", startTime);
-                if(startTime < endTime){
-                    _delay_ms(endTime - startTime);
-                }else{
-                    _delay_ms(startTime - endTime);
-                }
-                PORTB = EEPROM_Read(readAddress + 1);
+			PORTB = data[1];
+			notePlay++;
+			
             }
-            readAddress = 0;
+			_delay_ms(2000);
         }
+		    PORTB = 0;
     }
 }
